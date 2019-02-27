44f93e55c07729c350c5e5b15c4be698a05f8262
Nicholas Mills <nicholasrmills@icloud.com>
2019-02-27 16:15:36 +1100

    Passed all POST tests

--- app/controllers/rating_questions_controller.rb
+++ app/controllers/rating_questions_controller.rb
@@ -2,4 +2,30 @@ class RatingQuestionsController < ApplicationController
   def index
     @rating_questions = RatingQuestion.all
   end
+
+  def create
+    @rating_question = RatingQuestion.new(question_params)
+
+    if @rating_question.save 
+      send_response(201, serialize(@rating_question))      
+    else 
+      send_response(422, { 'errors' => @rating_question.errors.messages })
+    end
+  end
+
+  def send_response(status, body)
+    render json: body, status: status
+  end
+
+  def serialize(question)
+    {
+      id: question.id.to_s,
+      title: question.title, 
+      tag: question.tag
+    }
+  end
+
+  def question_params
+    params.require(:rating_question).permit(:title, :tag)
+  end
 end
--- spec/requests/index_spec.rb
+++ spec/requests/index_spec.rb
@@ -16,3 +16,46 @@ RSpec.describe "GET /rating_questions" do
     expect(first_question["title"]).to eq(question.title)
   end
 end
+
+
+RSpec.describe "POST /ratingQuestions" do
+
+  let(:new_title) { "Hello World" }
+  let(:new_tag) { "new tag" }
+  
+  context "when the request has a body" do
+    
+    it "returns a 201 Created" do
+      post "/rating_questions.json", params: { rating_question: {title: new_title, tag: new_tag }}
+      expect(response.status).to eq(201)
+    end
+
+    it "returns the new document" do
+      post"/rating_questions.json", params: { rating_question: { title: new_title, tag: new_tag }}
+      question = JSON.parse(response.body)
+      expect(question.is_a?(Hash)).to eq(true)
+      expect(question.key?("id")).to eq(true)
+      expect(question["title"]).to eq(new_title)
+      expect(question["tag"]).to eq(new_tag)
+    end
+  end
+
+  context "when the request has no body" do
+    it "returns a 400 Bad Request" do
+      expect{ post "/rating_questions.json", params: {} }.to raise_error ActionController::ParameterMissing
+    end
+  end
+
+  context "when the request has a blank title" do
+    it "returns a 422 Invalid Resource" do
+      post "/rating_questions.json", params: { rating_question: {title: "", tag: new_tag}}
+      expect(response.status).to eq(422)
+    end
+
+    it "shows errors that the title cannot be blank" do
+      post "/rating_questions.json", params: { rating_question: {title: "", tag: new_tag}}
+      error = JSON.parse(response.body)
+      expect(error).to eq({"errors" => {"title" => ["can't be blank"]}})
+    end
+  end
+end
