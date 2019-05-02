08a4aa403a658e76bb3270468e1e3969ac2f9b1f
nickrm97 <nicholasrmills@icloud.com>
2019-05-02 16:39:15 +1000

    Setting up Apollo client. Need to refactor javascript to include App component hierarchy

--- app/javascript/components/RatingQuestions/index.tsx
+++ app/javascript/components/RatingQuestions/index.tsx
@@ -3,6 +3,14 @@ import * as React from 'react'
 import axios from 'axios'
 import * as styles from './index.module.scss'
 import RatingQuestion from './RatingQuestion'
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql"
+});
 
 interface Question {
   id: string,
@@ -17,8 +25,12 @@ class RatingQuestions extends React.Component<RatingQuestionsProps, {}> {
 
   render() {
     return(
-      <div className={styles.list} data-automation-id='questions-list'>
-        {this.props.questions.map((question) => <RatingQuestion key={question.id} {...question} />)}
+      <div>
+        <ApolloProvider client={client}>
+          <div className={styles.list} data-automation-id='questions-list'>
+            {this.props.questions.map((question) => <RatingQuestion key={question.id} {...question} />)}
+          </div>
+        <ApolloProvider />
       </div>
     )
   }
